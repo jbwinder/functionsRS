8478f12914a0537ad481d509335ff0baf4978359
jbwinder <jbwinder@byu.edu>
2025-02-28 13:51:38 -0700

    edits

--- pick_teams.py
+++ pick_teams.py
@@ -1,7 +1,6 @@
 # Taylor Wall
 # Display an introduction to the game explaining rules and prompt for their name and display that in the welcome message
 # Return the name to the main program and store it in variable so it can be used throughout the program.
-
 def intro():
     name = input("Please enter your name: ")
 
@@ -31,16 +30,15 @@ def pick_teams(exclude_team = None):
         teams.remove(exclude_team)
 
     # print available teams for user to choose between.
-    print("\nAvailable Teams:\n")
+    print("Available Teams:")
     
     for team in teams:
         print(team)
-    print()
 
     # ask user to type the name of a team as seen above. If name does not match team in list then make them do ti again.
     # When typed name correctly return the name.
     while True:
-        choice = input("Write name of team you are choosing. Make sure to write name correctly with the correct capitalization. ").strip()
+        choice = input("Write name of team you are choosing: ")
         if choice in teams:
             return choice
         else:
@@ -66,18 +64,11 @@ def determine_winner(home_team, away_team):
     return results
 
 
-
-
-
-
-
 # Jakob Kahler
 # Part 5 of Functions Are US
 # Display the final record for a team. Receive the home team data and display information.
-
-
 def display_final_record(win_loss_log):
-# Display the final record for a team
+# find how many wins and losses the home team has
     wins= 0
     losses = 0
     for game in win_loss_log:
@@ -86,7 +77,7 @@ def display_final_record(win_loss_log):
         else:
             losses += 1
     print(f"\nFinal season record for {home_team}: {wins}-{losses}")
-# Receive home team data and display their season results
+# Decide how good the season was depending on win percentage.
     win_percentage = wins / (wins + losses)
     if win_percentage >= 0.75:
         print("Qualified for the NCAA Women's Soccer Tournament")
@@ -96,16 +87,20 @@ def display_final_record(win_loss_log):
         print("Your team needs to practice!")
 
 # main function
+# set list of W's and L's
 win_loss_log = []
 name = intro()
 print("Pick a home team:")
 home_team = pick_teams()
+# Set Loop to only break if they ask to leave loop
+# pick a home team and away team
 loop = True
 while loop == True:
     choice = callmenu()
     if choice == 1:
         print("Pick an away team:")
         away_team = pick_teams(home_team)
+# append the W or L to the list of win_loss_log
         results = determine_winner(home_team, away_team)
         win_loss_log.append(results)
     else:
